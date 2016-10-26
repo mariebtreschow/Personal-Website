a443b99079f6a1a62a2cf5a0ed7556646ec33a29
Marie Treschow <treschow.marie@gmail.com>
2016-10-26 15:53:58 +0200

    email works

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express'),
       morgan = require('morgan'),
       bodyParser = require('body-parser'),
       pug = require('pug'),
+      nodemailer = require('nodemailer'),
       app = express();
+
+
+var transporter = nodemailer.createTransport('smtps://treschow.marie%40gmail.com:cutest229967hh@smtp.gmail.com');
+
 
 app.use(express.static(__dirname + '/public'));
 
@@ -18,9 +23,28 @@ app.get('/', function(request, response) {
   response.send(pug.renderFile(__dirname + '/views/application.pug', {}));
 });
 
-app.post('/send-email', function(request, response) {
+app.post('/send-email', function(request, response){
   console.log('called the post route on express');
+  console.log('HTTP request body was:');
+  console.log(request.body);
+
+  transporter.sendMail({
+    from: 'treschow.marie@gmail.com',
+    to: 'treschow.marie@gmail.com',
+    subject: 'Email received from Personal Website',
+    text: request.body.email_content + '\n\nSender Email: ' +
+    request.body.emailAddress  + '\nSender Full Name: ' + request.body.fullName
+    }, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log('Message sent: ' + info.response);
+  });
+
+  response.redirect('/');
 });
+
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!!!');
