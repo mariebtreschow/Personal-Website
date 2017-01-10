b59c012cec41cc239f042097a8376516038f6165
Marie Treschow <treschow.marie@gmail.com>
2017-01-10 12:00:24 +0100

    small updates

--- index.js
+++ index.js
@@ -6,43 +6,41 @@ const express = require('express'),
       app = express();
 
 
-var transporter = nodemailer.createTransport('smtps://treschow.marie%40gmail.com:cutest229967hh@smtp.gmail.com');
+var transporter = nodemailer.createTransport('smtps://treschow.marie%40gmail.com:XXXXXXX@smtp.gmail.com');
 
 
 app.use(express.static(__dirname + '/public'));
-
 app.use(morgan('dev'));
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.get('/', function(request, response) {
+app.get('/', function(req, res) {
   console.log('Requesting homepage and contact form');
-  response.send(pug.renderFile(__dirname + '/views/application.pug', {}));
+  res.send(pug.renderFile(__dirname + '/views/application.pug', {}));
 });
 
-app.post('/send-email', function(request, response){
+app.post('/send-email', function(req, res){
   console.log('called the post route on express');
   console.log('HTTP request body was:');
-  console.log(request.body);
+  console.log(req.body);
 
   transporter.sendMail({
     from: 'treschow.marie@gmail.com',
     to: 'treschow.marie@gmail.com',
-    subject: 'Email received from Personal Website',
-    text: request.body.email_content + '\n\nSender Email: ' +
-    request.body.emailAddress  + '\nSender Full Name: ' + request.body.fullName
+    subject: 'Email from Personal Website',
+    text: req.body.email_content + '\n\nSender Email: ' +
+    req.body.emailAddress  + '\nSender Full Name: ' + req.body.fullName
     }, (error, info) => {
     if (error) {
       return console.log(error);
     }
 
-    console.log('Message sent: ' + info.response);
+    console.log('Message sent: ' + info.res);
   });
 
-  response.redirect('/');
+  res.redirect('/');
 });
 
 
