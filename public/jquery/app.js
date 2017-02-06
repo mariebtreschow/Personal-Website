9dfb13bf7c591f530ab25d0a63d55593cef35d58
Marie Treschow <treschow.marie@gmail.com>
2017-02-06 22:29:27 +0100

    removed hashed in url

--- public/jquery/app.js
+++ public/jquery/app.js
@@ -14,12 +14,8 @@ $(document).ready(function() {
       var target = this.hash;
       var $target = $(target);
 
-      //scroll and show hash
       $('html, body').animate({
          'scrollTop': $target.offset().top
-      }, 1000, 'swing', function() {
-         window.location.hash = target;
-      });
-
+      }, 1000, 'swing');
    });
 });
